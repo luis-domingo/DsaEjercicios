82ea80df628b0139a9278bd87f0f5279bb26b6f6
luis-domingo <72356764+luis-domingo@users.noreply.github.com>
2021-01-24 10:09:58 +0100

    Unity

---
+++ UnityTutorial/Roguelike-2D-master/Assets/Scripts/Enemy.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject {
+
+    public int playerDamage;
+    public AudioClip enemyAttack1;
+    public AudioClip enemyAttack2;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Transform target;
+    private bool skipMove;
+
+	// Use this for initialization
+	protected override void Start () {
+        GameManager.instance.AddEnemyToList(this);
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        if(skipMove) {
+            skipMove = false;
+            return;
+        }
+        base.AttemptMove<T>(xDir, yDir);
+        skipMove = true;
+    }
+
+    public void MoveEnemy()
+    {        
+        int xDir = 0;
+        int yDir = 0;
+        bool horizontal = false;
+        bool vertical = false;
+
+        if (Mathf.Abs(target.position.x - transform.position.x) > float.Epsilon)
+        {
+            horizontal = true;
+        }
+        if (Mathf.Abs(target.position.y - transform.position.y) > float.Epsilon)
+        {
+            vertical = true;
+        }
+
+        if(vertical) {
+            xDir = 0;
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+            if(CanMove(xDir,yDir)) {
+                AttemptMove<Player>(xDir, yDir);
+                return;
+            }
+        }
+
+        if (horizontal)
+        {
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+            yDir = 0;
+
+            // Flips sprite following horizontal direction
+            if(xDir < 0)
+            {
+                spriteRenderer.flipX = false;
+            } else
+            {
+                spriteRenderer.flipX = true;
+            }
+
+
+            if (CanMove(xDir, yDir))
+            {
+                AttemptMove<Player>(xDir, yDir);
+                return;
+            }
+
+        }
+
+
+    }
+    protected override void OnCantMove<T>(T component)
+    {
+        Player hitPlayer = component as Player;
+        animator.SetTrigger("enemyAttack");
+        SoundManager.instance.RandomizeSfx(enemyAttack1, enemyAttack2);
+        hitPlayer.LoseFood(playerDamage);
+    }
+}
---
+++ UnityTutorial/Roguelike-2D-master/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+
+    public float barDisplay = 100;
+    public Vector2 pos = new Vector2(20,40);
+    public Vector2 size = new Vector2(60,20);
+    public Texture2D progressBarEmpty;
+    public Texture2D progressBarFull;
+
+    private void OnGUI()
+    {
+        Debug.Log("BAR: " + barDisplay);
+
+        GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
+            GUI.Box(new Rect(20, 20, 50,50), progressBarEmpty);
+
+            //GUI.BeginGroup(new Rect(0, 0, size.x * barDisplay, size.y));
+            //    GUI.Box(new Rect(0, 0, size.x, size.y), progressBarFull);
+            //GUI.EndGroup();
+        
+        GUI.EndGroup();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        barDisplay -= Time.deltaTime * 10.0f;
+
+        if(barDisplay<=0) {
+            barDisplay = 100;
+        }
+
+
+    }
+}
